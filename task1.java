2a678ce0e89ab5ab0c1041d949081b363294a6a6
Vadim <vadim@Vadims-Mac-mini.local>
2022-06-27 15:40:47 +0400

    Task1

---
+++ task1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class task1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("введите число: ");
+        int n = iScanner.nextInt();
+        System.out.println(triangle(n));
+        }
+    static public int triangle(int n) { 
+        if(n==1)
+            return 1;
+        else
+            return( n + triangle(n-1));
+    
+    } 
+}
